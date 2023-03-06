40e813189e5cd7c8807103953fbcb2cd8660fd09
20221PF.CC0003 <20221PF.CC0003@PC-155976.passofundo.ifsul.local>
2023-03-06 11:06:52 -0300

    Implementação

---
+++ src/main/java/br/edu/ifsul/bcc/too/ProjetoMaicon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.too;
+
+/**
+ *
+ * @author 20221PF.CC0003
+ */
+public class ProjetoMaicon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
